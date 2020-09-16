ce4e2289b13fe3265d3918856b0c083e706838f6
Junichi Koyama <koyama@rightcode.co.jp>
2020-09-16 17:01:58 +0900

    Spring Security チュートリアル

---
+++ bookstore/grails-app/controllers/com/mycompany/myapp/SecureController.groovy
@@ -0,0 +1,8 @@
+package com.mycompany.myapp
+import grails.plugin.springsecurity.annotation.Secured
+class SecureController {
+    @Secured('ROLE_ADMIN')
+    def index() {
+        render 'Secure access only'
+    }
+}
