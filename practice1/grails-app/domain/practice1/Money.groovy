b5c873e778f24f0e67ad66a2a56f2c8e08cb45cd
Junichi Koyama <koyama@rightcode.co.jp>
2020-09-18 15:51:43 +0900

    Moneyドメイン中身作成

--- practice1/grails-app/domain/practice1/Money.groovy
+++ practice1/grails-app/domain/practice1/Money.groovy
@@ -1,7 +1,45 @@
 package practice1
 
+import java.time.LocalDateTime
+
 class Money {
 
+    /**
+     * ユーザーID
+     */
+    Integer userId
+
+    /**
+     * メモ
+     */
+    String memo
+
+    /**
+     * 金額
+     */
+    Integer money
+
+    /**
+     * 作成日時
+     */
+    LocalDateTime createTime = LocalDateTime.now()
+
+    /**
+     * 削除フラグ
+     */
+    Integer deleteFlag = 0
+
     static constraints = {
+    }
+
+    static belongsTo = [user: User]
+
+    // データベースにドメインクラスをマップする方法を設定する
+    static mapping = {
+        userId column: 'user_id'
+        memo column: 'memo'
+        money column: 'money'
+        createTime column: 'create_time'
+        deleteFlag column: 'delete_flag'
     }
 }
