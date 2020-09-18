4846084319940515fd86a16165ad222b70eb1183
Junichi Koyama <koyama@rightcode.co.jp>
2020-09-18 10:32:22 +0900

    ユーザードメイン設定

--- practice1/grails-app/domain/practice1/User.groovy
+++ practice1/grails-app/domain/practice1/User.groovy
@@ -1,7 +1,44 @@
 package practice1
 
+import java.time.LocalDateTime
+
 class User {
 
+    /**
+     * ユーザー名
+     */
+    String userName
+
+    /**
+     * ユーザーパスワード
+     */
+    String password
+
+    /**
+     * 作成日時
+     */
+    LocalDateTime createTime = LocalDateTime.now()
+
+    /**
+     * 最終更新日時
+     */
+    LocalDateTime updateTime = LocalDateTime.now()
+
+    /**
+     * 削除フラグ
+     */
+    Integer deleteFlag = 0
+
+    // 制約を設定する
     static constraints = {
+    }
+
+    // データベースにドメインクラスをマップする方法を設定する
+    static mapping = {
+        userName column: 'user_name'
+        password column: 'password'
+        createTime column: 'create_time'
+        updateTime column: 'update_time'
+        deleteFlag column: 'delete_flag'
     }
 }
